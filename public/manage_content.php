658cae856a8805953b9b38eef30a9a22708e443a
Zheng Li <zli@outlook.com>
2017-01-31 17:35:33 -0500

    Refactoring changes

--- public/manage_content.php
+++ public/manage_content.php
@@ -1,43 +1,39 @@
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-
-<?php 
-  // 2. Perform DB query
-  $query = "SELECT * ";
-  $query .= "FROM subjects ";
-  $query .= "WHERE visible = 1 ";
-  $query .= "ORDER BY position ASC";
-
-  $result = mysqli_query($connection, $query);
-  // query error
-  confirm_query($result);
-
- ?>
-
 <?php include("../includes/layout/header.php"); ?>
+
     <div id="main">
       <div id="navigation">
-        <ul class="subjects">
-		  <?php 
-		  // 3. User returned data
-		  while ($subject = mysqli_fetch_assoc($result)) {
-		    ?>
-		    <li> <?php echo $subject["menu_name"] . " (" . 
-		    $subject["id"] . ")"; ?></li>
+        <ul class="subjects">  
+        <?php $subject_set = find_all_subjects(); ?> 
+        <?php while ($subject = mysqli_fetch_assoc($subject_set)) { 
+        	?>
+        <li>
+        <?php echo $subject["menu_name"]; ?>
+		<?php $page_set = find_pages_for_subject($subject["id"]); ?>
+		<ul class="pages">
+			<?php 
+					while ($subject = mysqli_fetch_assoc($page_set)) {
+					 ?>
+					 	<li><?php echo $subject["menu_name"];?></li>
+					<?php 
+						}
+					?>
+					<?php mysqli_free_result($page_set); ?>
+		    	</ul>
+		    </li>
 		    <?php 
 		  }
-
 		   ?>
+
+    <?php mysqli_free_result($subject_set); ?>
+
 		   </ul>
       </div>
       <div id="page">
         <h2>Manage Content</h2>
       </div>
     </div>
-
-
-    <?php 
-   mysqli_free_result($result); ?>
   </body>
 </html>
   
